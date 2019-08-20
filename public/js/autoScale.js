3f0d9a3ec9fbf39df3d755e66a30023f28b02e1b
yangnan <yangnan@leadeon.cn>
2019-08-20 15:58:46 +0800

    fix:练习

---
+++ public/js/autoScale.js
@@ -0,0 +1,17 @@
+//等比缩放
+!(function(doc, win) {
+    var timer,
+        docEle = doc.documentElement,
+        evt = "onorientationchange" in window ? "orientationchange" : "resize",
+        setFontSize = function() {
+            var width = docEle.getBoundingClientRect().width;
+            width && (docEle.style.fontSize = 20 * (width / 320) + "px");
+        };
+
+    win.addEventListener(evt, function() {
+        clearTimeout(timer);
+        timer = setTimeout(setFontSize, 1000);
+    }, false);
+    setFontSize();
+    // doc.addEventListener("DOMContentLoaded", setFontSize, false);
+}(document, window));
