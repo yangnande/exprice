4ad8ee589f2d32e4467ce92c174f7665406c3bf6
yangnan <yangnan@leadeon.cn>
2019-11-19 09:57:57 +0800

    fix:exprice responsive data

--- vue-router2/src/App.vue
+++ vue-router2/src/App.vue
@@ -3,9 +3,13 @@
     <h2>父页面----state</h2>
     {{$store.state.counter}}
     <h2>父页面----getter</h2>
-    <!-- <p>{{$store.getters.more20Students}}</p> -->
-    <p>{{$store.getters.more20Students(15)}}</p>
+    <p>{{$store.getters.more20Students}}</p>
+    <!-- <p>{{$store.getters.more20Students(15)}}</p> -->
     <p>{{$store.getters.more20StudentLength}}</p>
+    <!-- state，对象修改响应式 -->
+    <p>state，对象修改响应式对象修改响应式</p>
+    <p>{{$store.state.info}}</p>
+    <button @click="addInfo">增加信息</button>
     <h2>子页面----</h2>
     <vuex/>
   </div>
@@ -17,6 +21,11 @@ export default {
   name: 'App',
   components: {
     vuex
+  },
+  methods: {
+    addInfo () {
+      this.$store.commit('updateInfo')
+    }
   }
 }
 </script>
--- vue-router2/src/components/vuex.vue
+++ vue-router2/src/components/vuex.vue
@@ -3,10 +3,15 @@
     <p>{{$store.state.counter}}</p>
     <button @click="add">+</button>
     <button @click="decrese">-</button>
+    <p> mutations-传递参数(一个值)</p>
+    <button @click="addFive(5)">+5</button>
+    <button @click="addFive(10)">+10</button>
     <h2>子页面----getter</h2>
-    <!-- <p>{{$store.getters.more20Students}}</p> -->
-    <p>{{$store.getters.more20Students(15)}}</p>
+    <p>{{$store.getters.more20Students}}</p>
+    <!-- <p>{{$store.getters.more20Students(15)}}</p> -->
     <p>{{$store.getters.more20StudentLength}}</p>
+    <p>mutations-传递参数(一个对象)</p>
+    <button @click="addStudents">添加学生信息</button>
   </div>
 </template>
 
@@ -18,6 +23,19 @@ export default {
     },
     decrese () {
       this.$store.commit('decrement')
+    },
+    addFive (count) {
+      // commit的参数（type,传递的值）提交方式一
+      // this.$store.commit('incrementFive', count)
+      // commit的参数（type,传递的值）提交方式二
+      this.$store.commit({
+        type: 'incrementFive',
+        count
+      })
+    },
+    addStudents () {
+      let students = {age: 18, name: 'lilei'}
+      this.$store.commit('addStu', students)
     }
   }
 }
