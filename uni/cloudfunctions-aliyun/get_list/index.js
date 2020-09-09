b11c648659b36afecc9bc05ac9f7d31ce6022f71
yangnande <593894541@qq.com>
2020-09-09 22:40:06 +0800

    uni-app聚合处理数据，切换懒加载数据方法

--- uni/cloudfunctions-aliyun/get_list/index.js
+++ uni/cloudfunctions-aliyun/get_list/index.js
@@ -3,7 +3,21 @@ const db = uniCloud.database()
 exports.main = async (event, context) => {
 	//event为客户端上传的参数
 	console.log('event : ', event)
-	const list = await db.collection('article').get()
+	const {name} = event
+	// 聚合：更精细化的去处理数据，求和，分组，指定那些字段
+	const list = await db.collection('article')
+	 // 获取数据库集合的聚合操作实例
+	.aggregate()
+	// 根据条件过滤文档，并且把符合条件的文档传递给下一个流水线阶段。 
+	.match({
+		classily: name
+	})
+	// 把指定的字段传递给下一个流水线，指定的字段可以是某个已经存在的字段，也可以是计算出来的新字段。 
+	// .project({
+		
+	// }) 
+	.end() // 标志聚合操作定义完成，发起实际聚合操作 
+	// const list = await db.collection('article').get()
 	//返回数据给客户端
 	return {
 		code: 200,
--- uni/components/list/list.vue
+++ uni/components/list/list.vue
@@ -1,7 +1,7 @@
 <template>
 	<swiper class="home-swiper" @change="indexChange" :current="activeIndex">
 		<swiper-item v-for="(item,index) in tab" :key="index" class="swiper-item" >
-			<list-item :listData="listData"></list-item>
+			<list-item :listData="listCatchData[index]"></list-item>
 		</swiper-item>
 	</swiper>
 </template>
@@ -25,11 +25,20 @@
 		},
 		data() {
 			return {
-				listData: []
+				listData: [],
+				// js限制
+				listCatchData: {}
 			};
 		},
+		watch:{
+			tab(newval) {
+				if(newval.length === 0) return
+				this.getList(this.activeIndex)
+			}
+		},
 		created() {
-			this.getList()
+			// tab 还没有拿到
+			// this.getList(0)
 		},
 		components: {
 			listItem
@@ -38,11 +47,16 @@
 			indexChange(e) {
 				let { current } = e.detail
 				this.$emit('change',current)
+				this.getList(current)
+				// console.log(this.tab,current,this.tab[current])
 			},
-			getList() {
-				this.$api.getList().then(res => {
-					// console.log(res)
-					this.listData = res.data
+			getList(current) {
+				this.$api.getList({name:this.tab[current].name}).then(res => {
+					// console.log('请求数据', res.data)
+					// this.listCatchData[current] = res.data
+					// 懒加载 只有需要哪部分的数据才加载哪部分的数据
+					this.$set(this.listCatchData,current,res.data)
+					// this.listData = res.data
 				})
 			}
 		}
