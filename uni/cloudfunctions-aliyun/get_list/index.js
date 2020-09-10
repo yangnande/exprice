413a127b16fe0e010b88d8a9a6c2ba5315bd7324
yangnan <yangnan@leadeon.cn>
2020-09-10 17:27:32 +0800

    uni-app首页实现加载更多功能

--- uni/cloudfunctions-aliyun/get_list/index.js
+++ uni/cloudfunctions-aliyun/get_list/index.js
@@ -3,19 +3,31 @@ const db = uniCloud.database()
 exports.main = async (event, context) => {
 	//event为客户端上传的参数
 	console.log('event : ', event)
-	const {name} = event
+	const {
+			name,
+			page = 1,
+			pageSize = 10
+		} = event
+	let matchObj = {}
+	if(name !== '全部') {
+		matchObj = {
+			classily: name
+		}
+	}
 	// 聚合：更精细化的去处理数据，求和，分组，指定那些字段
 	const list = await db.collection('article')
 	 // 获取数据库集合的聚合操作实例
 	.aggregate()
 	// 根据条件过滤文档，并且把符合条件的文档传递给下一个流水线阶段。 
-	.match({
-		classily: name
-	})
+	.match(matchObj)
 	// 把指定的字段传递给下一个流水线，指定的字段可以是某个已经存在的字段，也可以是计算出来的新字段。 
 	// .project({
 		
 	// }) 
+	// 指定一个正整数，跳过对应数量的文档，输出剩下的文档。
+	.skip(pageSize*(page -1))
+	// 限制输出到下一阶段的记录数
+	.limit(pageSize)
 	.end() // 标志聚合操作定义完成，发起实际聚合操作 
 	// const list = await db.collection('article').get()
 	//返回数据给客户端
